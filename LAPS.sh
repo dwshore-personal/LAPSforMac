d319d59cd6722cb24974eee42bd8d6453ddc597c
Christer Grafström <christer.grafstrom@ltu.se>
2019-09-18 07:29:10 +0200

    Fixed some spell error

--- LAPS.sh
+++ LAPS.sh
@@ -52,7 +52,7 @@ LogLocation="/private/var/log/$resetUser.log"
 # hardcode full path to jamf binary.
 jamf_binary="/usr/local/bin/jamf"
 
-major_version=sw_vers -productVersion | cut -f2 -d"."
+major_version=$(sw_vers -productVersion | cut -f2 -d".")
 
 ###################Create new Password#######################
 newPass=$(env LC_CTYPE=C tr -dc "A-Za-z0-9#\$^&_+=" < /dev/urandom | head -c 16;echo | echo "Aq1*")
@@ -176,7 +176,7 @@ if [ "$oldPass" = "" ];then
 else
   ScriptLogging "Updating password for $resetUser."
   echo "Updating password for $resetUser."
-  if [ $major_vesion == "12"; then
+  if [[ $major_vesion == "12" ]]; then
     sysadminctl -resetPasswordFor $resetUser -newPassword $newPass
   else
     sysadminctl -adminUser $resetUser -adminPassword $oldPass -resetPasswordFor $resetUser -newPassword $newPass
